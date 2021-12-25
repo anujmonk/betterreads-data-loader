1488fee1f429fa6f158acea3a5163676fea8419c
anujsharma <sharma.anuj15.github@gmail.com>
2021-12-24 20:22:44 -0500

    Setting up connection with cassandra

---
+++ src/test/java/io/project/betterreadsdataloader/BetterreadsDataLoaderApplicationTests.java
@@ -0,0 +1,13 @@
+package io.project.betterreadsdataloader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetterreadsDataLoaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
